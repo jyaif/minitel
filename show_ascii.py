40cd7e1025fe558be696fffa554969d063d0f100
Jean-François Geyelin <jfgeyelin@gmail.com>
2020-08-01 20:38:20 +0200

    Use SFW images. Interleave date printed with figlet.

--- show_ascii.py
+++ show_ascii.py
@@ -5,11 +5,13 @@ import random
 import os
 
 p = "ascii"
+p = "sfw_ascii"
 files = [f for f in listdir(p) if isfile(join(p, f))]
 
 print files
 
 while True:
+  os.system("date | figlet -f big > /dev/ttyUSB0")
   f = random.choice(files)
   cmd = "cat " + p + "/" + f + " > /dev/ttyUSB0"
   #print(cmd)
